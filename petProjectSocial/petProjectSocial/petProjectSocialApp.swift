5254b599cd3689b702e0e9238402ddf867e0391d
DmitriiKovrigin <dimals18@icloud.com>
2024-06-05 09:27:22 +0300

    Initial commit

---
+++ petProjectSocial/petProjectSocial/petProjectSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  petProjectSocialApp.swift
+//  petProjectSocial
+//
+//  Created by Dima Kovrigin on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct petProjectSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
